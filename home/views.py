76bb79d5ccfe5ede89249977c8325394e96ab45c
oguzhancokyilmaz <ocokyilmaz@gmail.com>
2020-04-30 23:26:01 +0300

    İletişim formu eklendi-çalışıyor

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.core.checks import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormMessage, ContactFormu
+from django.contrib import messages
 
 
 def index(request):
@@ -27,6 +29,21 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+    if request.method =='POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage() # Model ile bağlantı kur
+            data.name = form.cleaned_data['name'] #Bilgileri al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajiniz Gönderilmistir.")
+            return HttpResponseRedirect('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting,'page':'iletisim'}
+    form = ContactFormu()
+    context = {'setting': setting,'form':form}
     return render(request, 'iletisim.html', context)
