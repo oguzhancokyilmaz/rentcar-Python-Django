a67ff293c10555f74aa2c1fbe64d48aa42192808
oguzhancokyilmaz <ocokyilmaz@gmail.com>
2020-05-01 05:38:41 +0300

    Admin'de kategori'ye girememe hatası aldım list display'de image-tag leri kaldırarak hatayı çözdüm

--- product/models.py
+++ product/models.py
@@ -9,24 +9,19 @@ class Category(models.Model):
         ("True", "Evet"),
         ("False","Hayır"),
     )
-
-    title = models.CharField(max_length=30)
-    keywords = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    title = models.CharField(blank=True,max_length=30)
+    keywords = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=255)
     image = models.ImageField(blank=True,upload_to="images/")
-    status = models.CharField(max_length=10,choices=STATUS)
-
+    status = models.CharField(blank=True,max_length=10,choices=STATUS)
     slug = models.SlugField()
     parent = models.ForeignKey("self",blank=True,null=True,related_name="children",on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-
-
     def __str__(self):
         return self.title
-
     def image_tag(self):
-        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+       return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = "Image"
 
 
@@ -36,7 +31,7 @@ class Cars(models.Model):
         ("False","Hayır"),
     )
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
-    title = models.CharField(blank=True,max_length=30)
+    title = models.CharField(max_length=30)
     keywords = models.CharField(blank=True,max_length=255)
     description = models.CharField(blank=True,max_length=255)
     slug = models.SlugField(blank=True,max_length=255)
